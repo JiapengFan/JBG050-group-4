ac3aec20c199b25c213131d6b70f8298ddad4481
JiapengFan <j.fan@student.tue.nl>
2021-02-08 21:34:32 +0100

    Update parseData.py

--- parseData.py
+++ parseData.py
@@ -6,21 +6,22 @@ promotions_df = pd.read_csv('.\data\promotions.csv')
 transactions_df = pd.read_csv('.\data\\transactions.csv')
 
 class DataSet():
-""" Class for storing data and its attributes
-"""
+    """ Class for storing data and its attributes/properties
+    """
 
     def __init__(self, df, shape, columns):
         self.df = df
         self.shape = shape
         self.columns = columns
+        # add more useful attributes/properties
 
 inventory = DataSet(inventory_df, inventory_df.shape, inventory_df.columns)
 products = DataSet(products_df, products_df.shape, products_df.columns)
 promotions = DataSet(promotions_df, promotions_df.shape, promotions_df.columns)
 transactions = DataSet(transactions_df, transactions_df.shape, transactions_df.columns)
 
-# Inspect data set
-print(inventory)
-print(products)
-print(promotions)
-print(transactions)
+# Inspect data sets
+print(inventory.df)
+print(products.df)
+print(promotions.df)
+print(transactions.df)
