ab4e3a792f6bc170c2a238fb6ed61b02a5fbe19c
ottokaandorp <60935495+ottokaandorp@users.noreply.github.com>
2021-02-25 00:03:42 +0100

    Update total_products.py

--- productFunctions/total_products.py
+++ productFunctions/total_products.py
@@ -9,7 +9,7 @@ def total_products(data): #takes the dataframe of products before the delivery
     for s in df:
         for p in df.index:
             string = df[s][p]
-            list_string = re.findall(r'\d+', string)
+            list_string = re.findall(r'\d+', str(string))
             list_int = list(np.int_(list_string))
             relevant_values = [i for i in list_int if list_int.index(i)/2 - list_int.index(i)//2 != 0]
             total = sum(relevant_values)
